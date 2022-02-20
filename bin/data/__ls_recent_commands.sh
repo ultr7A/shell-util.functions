3924babb18921d0565afa0255a44ebc2fb5b90ef
Jeremy Evans <openspacehexagon@gmail.com>
2022-02-20 01:01:43 -0500

    re-organize

--- bin/data/__ls_recent_commands.sh
+++ bin/data/__ls_recent_commands.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 function __ls_recent_commands() {
-    cat ~/.bash_history | egrep -iE "$1"    
+    export entries="$2"
+    if [ -z $2 ];then
+	    export entries="1";
+	fi
+    echo $(cat ~/.bash_history | egrep -iE "$1" | tail -n "$entries")    
 }
