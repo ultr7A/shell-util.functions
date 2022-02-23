7f61b9705352d312721385f4a630de682f2d7583
Jeremy Evans <openspacehexagon@gmail.com>
2022-02-23 02:20:21 -0500

    fix output formatting of  __ls_recent_commands

--- src/data/__ls_recent_commands.sh
+++ src/data/__ls_recent_commands.sh
@@ -5,5 +5,5 @@ function __ls_recent_commands() {           ## SHELL_UTIL.FUNCTION ##
     if [ -z $2 ];then
 	    export entries="1";
 	fi
-    echo $(cat ~/.bash_history | egrep -iE "$1" | tail -n "$entries")    
+    cat ~/.bash_history | egrep -iE "$1" | tail -n "$entries" 
 }                                           ## SHELL_UTIL.FUNCTION ##
