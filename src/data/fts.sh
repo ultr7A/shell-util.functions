78b6631e63abbd0e9886f60b932253b0ec4c1192
Jeremy Evans <openspacehexagon@gmail.com>
2022-02-22 23:02:10 -0500

    improve 'fts' search command

--- src/data/fts.sh
+++ src/data/fts.sh
@@ -2,12 +2,26 @@
 #▔▔▔▔▔▔▔▔▔▔▔
 
 function fts() {				## SHELL_UTIL.FUNCTION ##	
-	leftCut=$3;
-	if [ -z $3 ];then
+	
+	export leftCut="$3";
+	export SEARCH_PATH="$2";
+
+	if [ -z "$3" ]
+	then
 		leftCut="25-";
+		if [ -z "$2" ]
+		then
+			SEARCH_PATH="$(pwd)";
+		fi
+	
 	fi
-   	clear 	                    &&
-   	find       $1 -type f -exec egrep -i -IH -A3 -B1   \
-                                     --color "'$2'" {} \;
+
+	if [ $# -eq 0 ]
+	then
+		echo "Command requires at minimum, a 'search phrase'."
+	fi
+
+   	#clear 	                    &&
+   	find       "$SEARCH_PATH" -type f -exec egrep -i -IH -A3 -B1 --color "'$1'" {} \;
 #        | cut   -c $leftCut;
 }								## SHELL_UTIL.FUNCTION ##
